b02ea13216110d765955cf43f2a070807355956f
ansellim <ansel@ansellim.com>
2022-10-28 23:50:11 +0800

    rough draft web app

---
+++ README.md
@@ -0,0 +1,9 @@
+# patientsurvey
+
+This is a repository for our project on patient feedback collection and analytics for the class CS6440 Intro to Health Informatics.
+
+Project team members:
+Ansel Lim
+Suyash Pandey
+Wallace Wong
+
---
+++ survey-app/src/App.js
@@ -0,0 +1,140 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {Survey} from 'survey-react-ui'
+
+// Use default theme for survey-react-ui npm package - import stylesheet
+import 'survey-core/modern.min.css'
+
+// import theme 
+import {StylesManager, Model} from 'survey-core';
+StylesManager.applyTheme('modern');
+
+// create model for survey
+const surveyQuestions = {
+  pages:[
+    {
+    elements:[
+      {
+        type: "html",
+        html: "<h2>Please provide information about the patient affected so we may help investigate the matter further.</h2>"
+      },
+      {
+        name: "PatientFirstName",
+        title: "First name:",
+        type: "text"
+      },
+      {
+        name: "PatientLastName",
+        title: "Last name:",
+        type: "text"
+      },
+      {
+        name: "BedNumber",
+        title: "Bed number:",
+        type: "text"
+      },
+      {
+        name: "Ward",
+        title: "Ward:",
+        type: "text"
+      }
+    ]
+  },
+  {
+    elements:[
+      {
+        type: "html",
+        html: "<h2>Please provide information about the feedback provider so we may respond to you where required.</h2>"
+      },
+      {
+        name: "FeedbackProviderFirstName",
+        title: "First name:",
+        type: "text"
+      },
+      {
+        name: "FeedbackProviderLastName",
+        title: "Last name:",
+        type: "text"
+      },
+      {
+        name: "FeedbackProviderRelationship",
+        title: "Relationship of feedback provider to the patient",
+        type: "dropdown",
+        colCount: 0,
+        showNoneItem: false,
+        choices: [
+          { value: "self", text: "self" },
+          { value: "family", text: "family" },
+          { value: "friend", text: "friend" },
+        ]
+      },
+      {
+        name: "FeedbackProviderContactNumber",
+        title: "Contact:",
+        type: "text",
+      },
+      {
+        name: "FeedbackProviderAddress",
+        title: "Address:",
+        type: "text",
+      }
+    ]
+  },
+  {
+    elements: [
+      {
+        type: "html",
+        html: "<h2>Please provide feedback on our doctors.</h2>"
+      },
+      {
+        name: "doctorsCourtesyRating",
+        title: "How courteous were our doctors on a scale of 1 (not at all) to 5 (very courteous)?",
+        type: "rating",
+        rateMin: 0,
+        rateMax: 5
+      },
+      {
+        name: "doctorsEmpathyRating",
+        title: "How empathetic were our doctors on a scale of 1 (not at all) to 5 (very empathetic)?",
+        type: "rating",
+        rateMin: 0,
+        rateMax: 5
+      },
+      {
+        name: "doctorsExplanationRating",
+        title: "How well did our doctors explain things to you on a scale of 1 (poor) to 5 (excellent)?",
+        type: "rating",
+        rateMin: 0,
+        rateMax: 5
+      },
+      {
+        name: "doctorsExplanationRating",
+        title: "How skillful were our doctors on a scale of 1 (not skillful) to 5 (highly skillful)?",
+        type: "rating",
+        rateMin: 0,
+        rateMax: 5
+      },
+      {
+        name: "doctorsExplanationRating",
+        title: "How professional were our doctors on a scale of 1 (not at all professional) to 5 (very professional)?",
+        type: "rating",
+        rateMin: 0,
+        rateMax: 5
+      }
+      ],
+  }
+    ]
+  }
+
+const survey = new Model(surveyQuestions);
+
+function App() {
+  return (
+    <div className="App">
+      <Survey model={survey}/>
+    </div>
+  );
+}
+
+export default App;
