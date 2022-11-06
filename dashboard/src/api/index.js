d52f20f1ff7e2f1b671330f85e7a979cfb554435
Ansel Lim <29158160+ansellim@users.noreply.github.com>
2022-11-06 22:42:42 +0800

    Delete dashboard directory

--- dashboard/src/api/index.js
+++
@@ -1,7 +0,0 @@
-// Connect to Express server and fetch the feedback items.
-
-import axios from 'axios';
-
-const url = "http://localhost:3006/feedback" // to connect to the Express server
-
-export const fetchFeedback = () => axios.get(url); // get feedback items from backend
--- dashboard/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-
-import feedbackItems from './feedbackItems';
-
-export default combineReducers({
-    feedbackItems
-});
