6327953005c5da12a82f5b00e04d2fe3f77361cc
ansellim <ansel@ansellim.com>
2022-11-06 23:43:41 +0800

    add net promoter score

---
+++ hospital-platform/src/components/cards/NetPromoterScore.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import GeneralCard from './GeneralCard';
+
+export const NetPromoterScore = () => {
+
+    const score = useSelector((state) => {
+
+        const feedbacks = state.feedbackItems;
+
+        console.log(feedbacks[45])
+
+        var num_feedbacks = 0;
+        var num_promoters = 0;
+        var num_detractors = 0;
+
+        for (let i = 0; i < feedbacks.length; i++) {
+            const feedback = feedbacks[i];
+
+            try {
+                const promotionRating = feedback.survey.overallrating.NetPromoterScore;
+
+                if (promotionRating >= 9) {
+                    num_promoters = num_promoters + 1;
+                } 
+                
+                if (promotionRating <= 6) {
+                    num_detractors = num_detractors + 1;
+                }
+                num_feedbacks = num_feedbacks + 1;
+
+            } catch {
+                continue;
+            }
+        }
+
+        const percentPromoters = num_promoters / num_feedbacks * 100;
+        const percentDetractors = num_detractors / num_feedbacks * 100;
+        const netPromoterScore = percentPromoters- percentDetractors;
+
+        console.log("net promoter score",netPromoterScore);
+
+        return { score: netPromoterScore, num_feedbacks: num_feedbacks }
+
+    });
+
+
+    return (
+        <>
+
+            <GeneralCard metricName="Net Promoter Score" score={Math.round(score.score)} numFeedback={score.num_feedbacks}></GeneralCard>
+
+        </>
+    )
+}
+
---
+++ hospital-platform/src/components/cards/SatisfactionScore.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import GeneralCard from './GeneralCard';
+
+export const SatisfactionScore = () => {
+
+    const score = useSelector((state) => {
+
+        const feedbacks = state.feedbackItems;
+
+        var satisfaction_scores = 0;
+        var num_feedbacks = 0;
+
+        for (let i = 0; i < feedbacks.length; i++) {
+            const feedbackItem = feedbacks[i];
+
+            try {
+                const satisfactionScore = feedbackItem.survey.overallrating.OverallSatisfaction;
+                satisfaction_scores = satisfaction_scores + satisfactionScore;
+                num_feedbacks = num_feedbacks + 1;
+            } catch {
+                continue;
+            }
+
+        }
+
+        const averageScore = satisfaction_scores / num_feedbacks;
+
+        return { average_score: averageScore , num_feedbacks: num_feedbacks }
+
+    });
+
+
+    return (
+        <>
+
+            <GeneralCard metricName="Overall Satisfaction" score={Math.round(score.average_score * 100) / 100} numFeedback={score.num_feedbacks}></GeneralCard>
+
+        </>
+    )
+}
+
