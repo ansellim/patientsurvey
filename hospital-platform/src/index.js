04dcf779e4bd303e4f3588d83b4e2069abe42a70
SphericalSilver <blueberrypudding85@gmail.com>
2022-11-13 17:39:15 +0800

    Graph additions

--- hospital-platform/src/index.js
+++ hospital-platform/src/index.js
@@ -5,15 +5,18 @@ import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 
 // redux
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers'
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import store from "./reducers";
+//const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 // Hooking the rreact app to the root div.
-ReactDOM.render(<Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-</Provider>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
